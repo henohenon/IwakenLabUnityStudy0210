c435cea102d6b30f208740f1093186ca67e0cc39
trapple <trapplejp@gmail.com>
2026-01-21 20:46:07 +0900

    Update scene and simplify Flow classes
    - Configure TutorialFlow with MouseInputObserver and DrawWeapon in scene
    - Simplify MainFlow to only handle TutorialFlow lifecycle
    - Remove unused code from BattleFlow, DrawFlow, WaitFlow, ResultFlow

--- IwakenLabUnityStudy01/Assets/Scripts/ResultFlow.cs
+++ IwakenLabUnityStudy01/Assets/Scripts/ResultFlow.cs
@@ -1,39 +1,8 @@
-using R3;
 using UnityEngine;
 
 namespace IwakenLabUnityStudy
 {
     public class ResultFlow : MonoBehaviour
     {
-        [SerializeField] private GameObject win;
-        [SerializeField] private GameObject lose;
-
-        private readonly Subject<Unit> _onFinish = new();
-        public Observable<Unit> OnFinish => _onFinish;
-
-        public void Initialize(bool isClear)
-        {
-            gameObject.SetActive(true);
-            // 表示対象のみアクティブ化
-            win.SetActive(isClear);
-            lose.SetActive(!isClear);
-
-            // アニメーション呼び出し
-            if (isClear)
-            {
-                var anim = win.GetComponent<TextObjectAnimator>();
-                anim?.SpawnAndShrink();
-            }
-            else
-            {
-                var anim = lose.GetComponent<TextObjectAnimator>();
-                anim?.SpawnAndShrink();
-            }
-        }
-
-        private void OnDestroy()
-        {
-            _onFinish.Dispose();
-        }
     }
 }
--- IwakenLabUnityStudy01/Assets/Scripts/TutorialFlow.cs
+++ IwakenLabUnityStudy01/Assets/Scripts/TutorialFlow.cs
@@ -6,66 +6,54 @@ namespace IwakenLabUnityStudy
 {
     public class TutorialFlow : MonoBehaviour
     {
-            [SerializeField] private XrObjectResolver xrObjectResolver;
-            [SerializeField] private OvrInputObserver ovrInputObserver;
-            [SerializeField] private GlobalData globalData;
+            [SerializeField] private MouseInputObserver mouseInputObserver;
             [SerializeField] private DrawWeapon drawWeapon;
-            [SerializeField] private TMPro.TMP_Text text;
-            [SerializeField] private Collider startObject;
-            [SerializeField] private TutorialLineController tutorialLineController;
+            //[SerializeField] private TMPro.TMP_Text text;
+            //[SerializeField] private TutorialLineController tutorialLineController;
 
             private readonly Subject<Unit> _finishTutorial = new ();
             public Observable<Unit> FinishTutorial => _finishTutorial;
 
             private WeaponDrawSequencer _ovrWeaponDraw;
-            private BattleWeapon _battleWeaponInstance;
             private IDisposable _drawSubscription;
             private IDisposable _startSubscription;
 
             private void Awake()
             {
-                if(!xrObjectResolver)
-                {
-                    throw new Exception("No XrObjectResolver found");
-                }
-
-                _ovrWeaponDraw = new WeaponDrawSequencer(ovrInputObserver, drawWeapon, xrObjectResolver, tutorialLineController);
+                _ovrWeaponDraw = new WeaponDrawSequencer(mouseInputObserver, drawWeapon);
 
                 _drawSubscription = _ovrWeaponDraw.OnDrawEnd.Subscribe(nodes =>
                 {
                     _drawSubscription?.Dispose();
 
-                    text.text = "ぶった斬れ！";
-                    xrObjectResolver.PenController.EnablePen(false);
+                    //text.text = "ぶった斬れ！";
                     drawWeapon.gameObject.SetActive(false);
-                    startObject.gameObject.SetActive(true);
 
-                    _battleWeaponInstance = Instantiate(globalData.prefabDictionary[WeaponStyle.Sword], xrObjectResolver.BattlePlayer.RightHand.transform);
-                    _battleWeaponInstance.Initialize(nodes, WeaponStyle.Sword, Utils.SelfLayer);
+                    // _battleWeaponInstance = Instantiate(globalData.prefabDictionary[WeaponStyle.Sword], xrObjectResolver.BattlePlayer.RightHand.transform);
+                    // _battleWeaponInstance.Initialize(nodes, WeaponStyle.Sword, Utils.SelfLayer);
 
-                    tutorialLineController.To = startObject.gameObject.transform;
-                    tutorialLineController.From = _battleWeaponInstance.transform;
-                    tutorialLineController.gameObject.SetActive(true);
+                    //tutorialLineController.To = startObject.gameObject.transform;
+                    //tutorialLineController.From = _battleWeaponInstance.transform;
+                    //tutorialLineController.gameObject.SetActive(true);
 
 
-                    _startSubscription?.Dispose();
-                    _startSubscription = _battleWeaponInstance.OnHit.Subscribe(col =>
-                    {
-                        if(!col.gameObject.CompareTag("StartObject")) return;
-                        tutorialLineController.gameObject.SetActive(false);
-                        Destroy(_battleWeaponInstance.gameObject);
-                        _ovrWeaponDraw?.Dispose();
-                        _ovrWeaponDraw = null;
-                        _finishTutorial.OnNext(Unit.Default);
-                        _startSubscription?.Dispose();
-                    }).AddTo(this);
+                    //_startSubscription?.Dispose();
+                    //_startSubscription = _battleWeaponInstance.OnHit.Subscribe(col =>
+                    //{
+                    //    if(!col.gameObject.CompareTag("StartObject")) return;
+                    //    tutorialLineController.gameObject.SetActive(false);
+                    //    Destroy(_battleWeaponInstance.gameObject);
+                    //    _ovrWeaponDraw?.Dispose();
+                    //    _ovrWeaponDraw = null;
+                    //    _finishTutorial.OnNext(Unit.Default);
+                    //    _startSubscription?.Dispose();
+                    //}).AddTo(this);
                 }).AddTo(this);
             }
 
             private void OnEnable()
             {
-                text.text = "一筆書きで剣を書け！";
-                startObject.gameObject.SetActive(false);
+                //text.text = "一筆書きで剣を書け！";
             }
     }
 }
